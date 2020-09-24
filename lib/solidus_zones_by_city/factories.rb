44eb80b295dcec7159e49dd6d7e53eab7e93487d
César Carruitero <ccarruitero@protonmail.com>
2020-09-24 12:03:58 -0500

    improve stock_location factory

--- lib/solidus_zones_by_city/factories.rb
+++ lib/solidus_zones_by_city/factories.rb
@@ -31,6 +31,21 @@ end
 
 FactoryBot.modify do
   factory :stock_location, class: 'Spree:StockLocation' do
-    city { create(:city, name: 'Seattle', state_code: 'WA', country_iso_code: 'US') }
+    transient do
+      city_name { 'Seattle' }
+      country_iso_code { 'US' }
+      state_code { 'WA' }
+    end
+
+    state do |stock_location|
+      Spree::State.joins(:country).where('spree_countries.iso = (?)', country_iso_code).find_by(abbr: state_code) ||
+        stock_location.association(:state, country_iso: country_iso_code, state_code: state_code)
+    end
+
+    city do |stock_location|
+      state = stock_location.state
+      Spree::City.find_by(name: city_name, state: state) ||
+      create(:city, name: city_name, state: state)
+    end
   end
 end
